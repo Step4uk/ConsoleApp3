46e78af0edf71a2fb9f97f8fb00ac45a44a84ef2
Kostin tip <kep4uk.for@gmail.com>
2025-11-11 10:38:54 +0300

    Выполнен расчет значения F.

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp3
             Console.Write("Введите значения параметра x =");
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
-
+            double F = 2 * Math.Cos(x);
         }
     }
 }
